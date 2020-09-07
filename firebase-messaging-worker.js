286323959c4a50c3e5b2a0077fa5dd2d90f08473
WhyIExist <hanishaarora05@gmail.com>
2020-09-08 02:40:56 +0530

    idk

--- firebase-messaging-worker.js
+++ firebase-messaging-worker.js
@@ -4,4 +4,22 @@ var config = {
     messagingSenderId: "576012749581",
 };
 firebase.initializeApp(config);
-const messaging = firebase.messaging()
+firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
+messaging.setBackgroundMessageHandler(function(payload) {
+    console.log(' Received background message ', payload);
+    var sender = JSON.parse(payload.data.message);
+    var notificationTitle = 'CometChat Pro Notification';
+    var notificationOptions = {
+        body: payload.data.alert,
+        icon: sender.data.entities.sender.entity.avatar,
+    };
+    return self.registration.showNotification(
+        notificationTitle,
+        notificationOptions
+    );
+});
+self.addEventListener('notificationclick', function(event) {
+    event.notification.close();
+    //handle click event onClick on Web Push Notification
+});
